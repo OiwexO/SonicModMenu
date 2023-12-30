b0a6cc8d1066313b54984ee665e28eaa358598c8
OiwexO <denyskostetskyi@gmail.com>
2023-12-30 19:24:52 +0200

    Sonic1Offsets.h update

--- app/src/main/cpp/cheat/memory/Sonic1Offsets.h
+++ app/src/main/cpp/cheat/memory/Sonic1Offsets.h
@@ -5,12 +5,11 @@
 namespace Offsets {
 	using OFFSET = unsigned int;
 
-	constexpr OFFSET Score = 0x28;
-	constexpr OFFSET Lives = 0x78;
-	constexpr OFFSET Rings = 0x7C;
+	constexpr OFFSET Score = 0x2545154;
+	constexpr OFFSET Lives = 0x254515C;
+	constexpr OFFSET Rings = 0x1F1882C;
 
-	constexpr OFFSET Shield = 0x10;
-	constexpr OFFSET Invincibility = 0x38;
-	constexpr OFFSET SuperForm = 0x50;
-
+	constexpr OFFSET Shield = 0x1F188C0;
+	constexpr OFFSET Invincibility = 0x1F18848;
+	constexpr OFFSET SuperForm = 0x1C467E0;
 }
